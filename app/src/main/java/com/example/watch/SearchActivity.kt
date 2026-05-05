c261024a5d2a59fe4600493c3544a41bc6fc9a27
tommykorch <tkorchmit@bk.ru>
2026-05-05 18:45:41 +0700

    Searching and search screen

--- app/src/main/java/com/example/watch/SearchActivity.kt
+++ app/src/main/java/com/example/watch/SearchActivity.kt
@@ -1,4 +1,49 @@
 package com.example.watch
 
-class SearchActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.watch.data.*
+import com.example.watch.databinding.ActivitySearchMovieBinding
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SearchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchMovieBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rcView.layoutManager = LinearLayoutManager(this)
+        val query = intent.getStringExtra("QUERY") ?: ""
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.omdbapi.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(OmdbResponse::class.java)
+
+        lifecycleScope.launch {
+            try {
+                val response = api.searchMovies(query)
+                if (response.Search != null) {
+                    binding.rcView.adapter = SearchAdapter(response.Search) { movie ->
+                        val resultIntent = Intent().apply {
+                            putExtra("title", movie.Title)
+                            putExtra("year", movie.Year)
+                            putExtra("poster", movie.Poster)
+                            putExtra("imdb", movie.imdbID)
+                        }
+                        setResult(RESULT_OK, resultIntent)
+                        finish()
+                    }
+                }
+            } catch (e: Exception) { e.printStackTrace() }
+        }
+    }
 }
