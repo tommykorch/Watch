4676ea2c733a76812bc053df263abfadcb9898c3
tommykorch <tkorchmit@bk.ru>
2026-05-19 13:05:46 +0700

    Fix database for compose

--- app/src/main/java/com/example/watch/data/MovieDao.kt
+++ app/src/main/java/com/example/watch/data/MovieDao.kt
@@ -1,19 +1,27 @@
 package com.example.watch.data
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MovieDao {
     @Query("SELECT * FROM movies")
-    fun getAllMovies(): LiveData<List<Movie>>
+    fun getAllMovies(): Flow<List<Movie>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(movie: Movie)
 
+    @Delete
+    suspend fun delete(movie: Movie)
+
+    @Query("DELETE FROM movies WHERE imdbID = :imdbId")
+    suspend fun deleteById(imdbId: String)
+
+
     @Query("DELETE FROM movies WHERE isSelectedForDelete = 1")
     suspend fun deleteSelected()
 
-    @Query("UPDATE movies SET isSelectedForDelete = :selected WHERE id = :id")
-    suspend fun updateSelection(id: Int, selected: Boolean)
+
+    @Query("UPDATE movies SET isSelectedForDelete = :selected WHERE imdbID = :imdbId")
+    suspend fun updateSelection(imdbId: String, selected: Boolean)
 }
