be53965dc23ca1ae5f0cd3e6fdb856f150ea60d4
tommykorch <tkorchmit@bk.ru>
2026-05-19 13:06:11 +0700

    New client Retrofit

---
+++ app/src/main/java/com/example/watch/data/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.example.watch.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://www.omdbapi.com/"
+
+    val api: OmdbResponse by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(OmdbResponse::class.java)
+    }
+}
